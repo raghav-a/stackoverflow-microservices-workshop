f42f658b7b1aa9b869da1c01839abf171c0b363d
Hari Krishnan <harikrishnan83@gmail.com>
2018-02-11 17:12:59 +0530

    adding questions route to gateway service

--- questions/src/main/java/com/agilefaqs/stackoverflow/questions/QuestionsApplication.java
+++ questions/src/main/java/com/agilefaqs/stackoverflow/questions/QuestionsApplication.java
@@ -2,13 +2,14 @@ package com.agilefaqs.stackoverflow.questions;
 
 import com.agilefaqs.stackoverflow.questions.dao.HashMapQuestionsDao;
 import com.agilefaqs.stackoverflow.questions.dao.QuestionsDao;
-import com.agilefaqs.stackoverflow.questions.model.Question;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class QuestionsApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuestionsApplication.class);
