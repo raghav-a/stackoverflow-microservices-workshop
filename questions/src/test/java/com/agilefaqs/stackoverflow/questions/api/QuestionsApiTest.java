afd17840b76001f5b5bec278881633c50cf65549
raghav.ag <raghav.ag@zeta.tech>
2018-03-01 15:09:59 +0530

    adding test stub for questions

---
+++ questions/src/test/java/com/agilefaqs/stackoverflow/questions/api/QuestionsApiTest.java
@@ -0,0 +1,28 @@
+package com.agilefaqs.stackoverflow.questions.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionsApiTest {
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void upvote() throws Exception {
+    }
+
+    @Test
+    public void downvote() throws Exception {
+    }
+
+}
