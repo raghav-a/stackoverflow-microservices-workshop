6b82e1c2fb6b78ce770f53c6d442a059d92682d2
raghav.ag <raghav.ag@zeta.tech>
2018-03-01 13:59:01 +0530

    removing unwated stuff for this iteration

--- questions/src/main/java/com/agilefaqs/stackoverflow/questions/servcie/QuestionsService.java
+++ questions/src/main/java/com/agilefaqs/stackoverflow/questions/servcie/QuestionsService.java
@@ -2,8 +2,6 @@ package com.agilefaqs.stackoverflow.questions.servcie;
 
 import com.agilefaqs.stackoverflow.questions.dao.QuestionsDao;
 import com.agilefaqs.stackoverflow.questions.model.Question;
-import com.agilefaqs.stackoverflow.questions.queues.Queues;
-import com.agilefaqs.stackoverflow.questions.queues.Queues.Channel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +9,10 @@ import org.springframework.stereotype.Component;
 public class QuestionsService {
 
     private QuestionsDao questionsDao;
-    private Channel<Question> questionsChannel;
 
 
     @Autowired
-    public QuestionsService(QuestionsDao questionsDao, Queues queues) {
-        questionsChannel = queues.createChannelForTopic("Posted_Questions");
+    public QuestionsService(QuestionsDao questionsDao) {
         this.questionsDao = questionsDao;
     }
 
@@ -27,14 +23,12 @@ public class QuestionsService {
 
     public String save(Question question) {
         questionsDao.save(question);
-        questionsChannel.publish(question);
         return question.getId();
     }
 
     public void update(String questionId, Question input) {
         input.setId(questionId);
         questionsDao.save(input);
-        questionsChannel.publish(input);
     }
 
     public void upvote(String questionId) {
