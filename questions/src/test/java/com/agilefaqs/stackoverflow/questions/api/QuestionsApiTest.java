3e209b536ae611111fdb856d299cd4868d221eed
raghav.ag <raghav.ag@zeta.tech>
2018-03-01 15:34:48 +0530

    adding tests and remomving cloud dependencies

--- questions/src/test/java/com/agilefaqs/stackoverflow/questions/api/QuestionsApiTest.java
+++ questions/src/test/java/com/agilefaqs/stackoverflow/questions/api/QuestionsApiTest.java
@@ -1,12 +1,50 @@
 package com.agilefaqs.stackoverflow.questions.api;
 
+import com.agilefaqs.stackoverflow.questions.controllers.QuestionsController;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class QuestionsApiTest {
+
+    @Autowired
+    private QuestionsController questionsController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+            .standaloneSetup(questionsController)
+            .build();
+    }
+
     @Test
-    public void get() throws Exception {
+    public void getQuestions() throws Exception {
+        mockMvc.perform(get("/questions/1"))
+            .andExpect(status().isOk());
+        mockMvc.perform(get("/questions/2"))
+            .andExpect(status().isOk());
+        mockMvc.perform(get("/questions/3"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getInvalidQuestions() throws Exception {
+        mockMvc.perform(get("/questions/4"))
+            .andExpect(status().isOk());
+
     }
 
     @Test
