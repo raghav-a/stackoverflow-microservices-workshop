2a058cffc18ffc08cad1f158bf0a2e38112324cc
raghav.ag <raghav.ag@zeta.tech>
2018-03-02 17:39:09 +0530

    * fixing issues of timeout at gateway

--- gateway-service/src/main/java/com/agilefaqs/stackoverflow/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/agilefaqs/stackoverflow/gateway/GatewayApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
@@ -27,7 +28,7 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
 @EnableZuulProxy
 @RestController
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableCaching
 public class GatewayApplication {
 
